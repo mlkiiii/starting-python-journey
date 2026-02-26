2411306995c0817bda26ff4323730107937bf0e5
malki mohamed <malkimohamed200506@gmail.com>
2026-02-26 17:14:59 +0100

    added new loop while

--- bagels/bagels.py
+++ bagels/bagels.py
@@ -3,6 +3,9 @@ finalx=randint(100,999)
 print("Welcome to Bagels, a deductive logic game.")
 print("I am thinking of a three-digit number. Try to guess what it is.")
 x=int(input("Enter your guess: "))
+while x<100 or x>999:
+    print("Please enter a valid three-digit number.")
+    x=int(input("Enter your guess: "))
 f=0
 while x!=finalx:
     if x//100==finalx//100:
@@ -27,4 +30,7 @@ while x!=finalx:
         print("bagels")
     f=0
     x=int(input("Enter your guess: "))
+    while x<100 or x>999:
+        print("Please enter a valid three-digit number.")
+        x=int(input("Enter your guess: "))
 print("Congratulations! You guessed the number!")
