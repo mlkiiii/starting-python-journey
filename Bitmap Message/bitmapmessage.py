78ba1d081c40aa40f1ac431440444597bcc77bba
malki mohamed <malkimohamed200506@gmail.com>
2026-02-27 04:07:40 +0100

    changed the structure of the program so it can input proparly and outpout proparly

--- Bitmap Message/bitmapmessage.py
+++ Bitmap Message/bitmapmessage.py
@@ -6,25 +6,28 @@ def multiline_input():
         if line == "":
             break
         lines.append(line)
-    return "\n".join(lines)
+    return lines
 def insertmessage():
     message=str(input("Enter your message: "))
     return message
-def bitmap_message(multiline_message,message):
+def bitmap_message(lines,message):
     print("Your message:")
-    print(multiline_message)
-    multiline_message_length=len(multiline_message)
-    bitmaped_message=""
-    message_length=len(message)
-    j=0
-    for i in range(multiline_message_length):
-        if multiline_message[i]!=" ":
-            bitmaped_message+=message[j]
-        else:
-            bitmaped_message+=" "
-        if j==message_length-1:
-            j=0
-        else:
-            j+=1
-    print(bitmaped_message)
+    for line in lines:
+        print(line)
+    for line in lines:
+        multiline_message=line
+        multiline_message_length=len(multiline_message)
+        message_length=len(message)
+        j=0
+        for i in range(multiline_message_length):
+            if multiline_message[i]!=" ":
+                multiline_message=multiline_message[:i]+message[j]+multiline_message[i+1:]
+                if j==message_length-1:
+                    j=0
+                else:
+                    j+=1
+        lines[lines.index(line)]=multiline_message
+    print("Bitmaped message:")
+    for line in lines:
+        print(line)
 bitmap_message(multiline_input(),insertmessage())
